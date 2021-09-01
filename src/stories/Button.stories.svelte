9ba25b932286a385f66e2ed3d742c3b24ce56932
soyrbto <soyrbto@gmail.com>
2021-09-01 13:54:34 -0400

    first commit

---
+++ src/stories/Button.stories.svelte
@@ -0,0 +1,57 @@
+<script>
+  import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
+  import Button from './Button.svelte';
+</script>
+
+<Meta
+  title="Tetrabids/Button"
+  component={Button}
+  argTypes={{
+    label: { control: 'text' },
+    primary: { control: 'boolean' },
+    color: {
+      control: {
+        type: 'select',
+        options: ['orange', 'purple', 'white', 'blue'],
+      },
+    },
+    onClick: { action: 'onClick' },
+  }}
+/>
+
+<Template let:args>
+  <Button {...args} on:click={args.onClick} />
+</Template>
+
+<Story
+  name="Default"
+  args={{
+    primary: true,
+    label: 'Button',
+  }}
+/>
+
+<Story
+  name="Default purple"
+  args={{
+    color: 'purple',
+    label: 'Button',
+  }}
+/>
+
+<Story
+  name="Outline purple"
+  args={{
+    primary: false,
+    color: 'purple',
+    label: 'Button',
+  }}
+/>
+
+<Story
+  name="Outline orange"
+  args={{
+    primary: false,
+    label: 'Button',
+  }}
+/>
---
+++ src/stories/Button.svelte
@@ -0,0 +1,74 @@
+<script>
+  import { createEventDispatcher } from 'svelte';
+
+  export let primary = true;
+  export let label = 'Button';
+  export let color = 'orange';
+
+  let mode = primary ? '' : 'outline';
+
+  const dispatch = createEventDispatcher();
+
+  function handleClick(event) {
+    console.log('I do not do anything yet');
+    dispatch('click', event);
+  }
+</script>
+
+<button on:click={handleClick} class={`${color}  ${mode}`}> {label} </button>
+
+<style>
+  button {
+    font-weight: 800;
+    border-radius: 15px;
+    height: 40px;
+    font-weight: 600;
+    color: white;
+    border-style: none;
+    padding: 10px 30px;
+    box-shadow: 0 5px 10px rgba(0, 0, 0, 0.2);
+    transition: all cubic-bezier(0.165, 0.84, 0.44, 1) 0.65s;
+    outline: none;
+    cursor: pointer;
+  }
+
+  /* COLORS BACKGROUND */
+
+  .orange {
+    background: linear-gradient(180deg, #ffbe0b 0%, #ab2626 100%);
+  }
+
+  .purple {
+    background: linear-gradient(180deg, #ec35f8 0%, #3c26ab 100%);
+  }
+
+  .white {
+    background: white;
+    color: #066d92;
+  }
+
+  .blue {
+    background: #11a7dd;
+    color: white;
+  }
+
+  /* OUTLINE BUTTON */
+
+  .outline {
+    color: black;
+    position: relative;
+    border: solid 2px transparent;
+    box-shadow: 2px 1000px 1px #fff inset;
+    background-origin: border-box;
+  }
+
+  .outline.orange {
+    color: #ab2626;
+  }
+
+  .outline.white {
+    border: solid 2px #066d92;
+    box-shadow: 0 5px 10px rgba(0, 0, 0, 0.2);
+    color: #066d92;
+  }
+</style>
